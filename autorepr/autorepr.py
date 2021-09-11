422d4ab1889b58e8e17a3d7b9ac79547c3606349
Chris Bouchard <chris@upliftinglemma.net>
2021-09-11 02:07:00 -0400

    Add support for styles
    For now, '()' (call style) and '<>' (angle style). As we walk the MRO
    list, we'll pick the first format we find. No complaints about conflicts
    -- it's valid for a subtype to override a supertype.

--- autorepr/autorepr.py
+++ autorepr/autorepr.py
@@ -1,16 +1,32 @@
 import types
 
 
-__all__ = ["AutoRepr", "autorepr"]
+__all__ = ["autorepr"]
 
 
-def autorepr(attributes_fn):
-    return AutoRepr(attributes_fn)
+def autorepr(attributes_fn, *, style=None):
+    return AutoRepr(attributes_fn, style)
+
+
+def angle_style(klass_name, attributes):
+    formatted_args = " ".join(f"{k}={v!r}" for k, v in attributes)
+    if formatted_args:
+        formatted_args = " " + formatted_args
+    return f"<{klass_name}{formatted_args}>"
+
+
+def call_style(klass_name, attributes):
+    formatted_args = ", ".join(f"{k}={v!r}" for k, v in attributes)
+    return f"{klass_name}({formatted_args})"
 
 
 class AutoRepr:
-    def __init__(self, attributes_fn):
+    default_style = call_style
+
+    def __init__(self, attributes_fn, style):
         self.attributes_fn = attributes_fn
+        self.style = style
+
         self.__doc__ = attributes_fn.__doc__
         self.__module__ = attributes_fn.__module__
         self.__name__ = attributes_fn.__name__
@@ -27,6 +43,7 @@ class AutoRepr:
     def __call__(self, instance):
         super_types = type(instance).__mro__
         attributes = []
+        style_fn = None
 
         for super_type in super_types:
             super_repr = super_type.__repr__
@@ -34,10 +51,21 @@ class AutoRepr:
             if not isinstance(super_repr, AutoRepr):
                 continue
 
+            if super_repr.style is not None and style_fn is None:
+                style_fn = self._resolve_style(super_repr.style)
+
             new_names = super_repr.attributes_fn(instance)
             attributes[0:0] = ((name, getattr(instance, name)) for name in new_names)
 
-        klass = type(instance).__qualname__
+        if style_fn is None:
+            style_fn = self._resolve_style(self.default_style)
 
-        formatted_args = ", ".join(f"{k}={v!r}" for k, v in attributes)
-        return f"{klass}({formatted_args})"
+        klass_name = type(instance).__qualname__
+        return style_fn(klass_name, attributes)
+
+    def _resolve_style(self, style):
+        if style == "<>":
+            return angle_style
+        elif style == "()":
+            return call_style
+        return style
