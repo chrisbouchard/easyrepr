e8d513bc3ea35ff48de2cee410a70d8560eb21e9
Chris Bouchard <chris@upliftinglemma.net>
2021-12-09 23:08:30 -0500

    Replace bottom_up parameter with top_down instance attribute
    It makes sense to configure this for the mirror instance, so that
    reflect_attributes can use the configured direction when iterating classes for
    slots.
    
    We've switched to top_down as the option instead of bottom_up, so that the
    default can be True like hide_private. I'm still not convinced a boolean makes
    sense here -- there are two options, but maybe an enum? I'm also not sure it
    matters enough to spend time on this bikeshed.

--- easyrepr/reflection.py
+++ easyrepr/reflection.py
@@ -12,23 +12,25 @@ class Mirror:
     :param hide_private: do not include private (according to
       :any:`is_private`) attributes when reflecting attributes. Default is
       `True`.
+    :param top_down: if `True`, reflect classes from top (most base, usually
+        :any:`object`) to bottom (most derived, i.e., the type of the
+        instance); otherwise from bottom to top. Default is `True` (top to
+        bottom).
     """
 
-    def __init__(self, hide_private=True):
+    def __init__(self, hide_private=True, top_down=True):
         self.hide_private = hide_private
+        self.top_down = top_down
 
-    def reflect_classes(self, instance, bottom_up=False):
+    def reflect_classes(self, instance):
         """Return all classes in the method resolution order (MRO) for the
         given instance's type.
 
         :param instance: the object whose classes should be reflected
-        :param bottom_up: if `True`, return classes from bottom (most derived)
-          to top (most base, usually :any:`object`); otherwise from top to
-          bottom. Default is `False` (top to bottom).
         """
         classes_bottom_up = type(instance).__mro__
 
-        if bottom_up:
+        if not self.top_down:
             return classes_bottom_up
 
         return reversed(classes_bottom_up)
@@ -41,8 +43,9 @@ class Mirror:
         attributes = []
 
         for klass in self.reflect_classes(instance):
-            if hasattr(klass, "__slots__"):
-                attributes.extend(self._filter_private_attributes(klass.__slots__))
+            slots = klass.__dict__.get("__slots__", None)
+            if slots is not None:
+                attributes.extend(self._filter_private_attributes(slots))
 
         if hasattr(instance, "__dict__"):
             attributes.extend(self._filter_private_attributes(instance.__dict__.keys()))
@@ -60,4 +63,4 @@ class Mirror:
     def __repr__(self):
         # No easy way to get EasyRepr in here. "I guide others to a treasure I
         # cannot possess."
-        return f"Mirror(skip_private={self.hide_private})"
+        return f"Mirror(skip_private={self.hide_private}, top_down={self.top_down})"
